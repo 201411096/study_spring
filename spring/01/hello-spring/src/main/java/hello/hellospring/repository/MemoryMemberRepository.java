61e85739ee92e0cf79c7df8a6a9956eb5e0bf173
201411096 <korea5781@gmail.com>
2021-03-11 12:05:51 +0900

    210311_03 junit test aftereach

--- spring/01/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ spring/01/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -42,4 +42,8 @@ public class MemoryMemberRepository implements MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore(){
+        store.clear();
+    }
 }
