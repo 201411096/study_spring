6e51fd840dcde72db9b7d11aee1f443fe284f2ca
201411096 <korea5781@gmail.com>
2021-03-18 11:28:15 +0900

    210318_01 spring security

--- spring/03/spring-quickstart/src/main/java/com/example/demo/DemoApplication.java
+++ spring/03/spring-quickstart/src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,14 @@
 /*
-Building a RESTful webService
-Consuming a RESTful Web Service
-Enabling Cross Origin Requests for a RESTful Web Service
+Securing a Web Application
  */
 
 package com.example.demo;
 
-import com.example.demo.consumingrest.Quote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -42,8 +33,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value="name", defaultValue = "World") String name){
-		return String.format("Hello %s!", name);
-	}
 }
