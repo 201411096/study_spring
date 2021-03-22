48de29dbbfd973fec60e91293e537925fe93a57f
201411096 <korea5781@gmail.com>
2021-03-22 17:14:38 +0900

    210322_01

--- spring/04/core/src/main/java/hello/core/AppConfig.java
+++ spring/04/core/src/main/java/hello/core/AppConfig.java
@@ -41,21 +41,42 @@ import org.springframework.context.annotation.Configuration;
 //    }
 //}
 
-@Configuration
+@Configuration // -> 바이트코드를 조작하는 CGLIB 기술을 사용해서 싱글톤을 보장 <-> @Bean은 보장을 해주지 않음
 public class AppConfig {
+    // factoryMethod를 이용하는 방식..
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderSerivce -> new MemoryMemberRepository(), ...
+    // L singleton이 꺠지는 것처럼 보임
+
+    // 예상
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+
+    // 결과
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
+    // Typically, @Bean methods are declared within @Configuration classes.
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
--- spring/04/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ spring/04/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
+    // 의존관계를 자동으로 주입해줌 MemberRepository에 해당하는 ... 것을 자동으로 등록
+    @Autowired // ac.getBean(MemberRepository.class);
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
@@ -17,5 +23,10 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    //test
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
     }
 }
