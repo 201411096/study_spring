1c7b7634754efc771299a1c19611a9ed01d5c921
201411096 <korea5781@gmail.com>
2021-04-15 10:38:49 +0900

    210415_01 composite pattern

---
+++ java/02_design-pattern/10_CompositePattern/src/Folder.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//composite
+public class Folder extends Component{
+	
+	public Folder(String name) {
+		super(name);
+	}
+
+	List<Component> children = new ArrayList<Component>();
+	
+	public boolean addComponent(Component component) {
+		return children.add(component);
+	}
+	
+	public boolean removeComponent(Component component) {
+		return children.remove(component);
+	}
+	
+	public List<Component> getChildren() {
+		return children;
+	}
+}
